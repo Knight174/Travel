558fd66ba06695f63ca41fe4f5627e9c9e2de857
Knight174 <425302118@qq.com>
2020-05-24 16:53:05 +0800

    change

--- README.md
+++ README.md
@@ -19,3 +19,19 @@ npm run build --report
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+# 初始化（都在main.js中操作）
+1. 在main.js中引基础样式和解决1像素问题的样式（reset.css border.css）
+```
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+```
+
+2. 解决移动端300ms点击事件延迟问题（npm install fastclick --save）
+```
+import fastClick from 'fastclick'
+fastClick.attach(document.body)
+```
+
+3. 在iconfont网上新建项目文件夹 ==>
+https://www.iconfont.cn/
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for='item of swiperList' :key='item.id'>
+        <img class="swiper-img" :src="item.imgUrl"/>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true // 控制轮播插件支持循环轮播
+      },
+      swiperList: [
+        {id: '0001', imgUrl: 'http://img1.qunarzz.com/sight/p0/2003/6c/6cd013b870474899a3.img.jpg_350x240_91f1395b.jpg'},
+        {id: '0002', imgUrl: 'http://img1.qunarzz.com/sight/p0/1803/ae/ae876a8017aeadefa3.img.jpg_350x240_a891c2c3.jpg'}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+      background #fff
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 4rem
+    background-color #eee
+    .swiper-img
+      width 100%
+      height 4rem
+</style>
